866ecf5573335c3b743879d42e03283313c6fa29
Carlos Rodriguez <carlos@s8f.org>
2016-05-06 19:50:06 -0700

    use new console

--- middleware/closeBuffet.js
+++ middleware/closeBuffet.js
@@ -2,9 +2,9 @@ module.exports = function container (get, set) {
   return function closeBuffet (cb) {
     var buffet_instance = get('middleware.buffet_instance')
     if (buffet_instance) {
-      get('vendor.console').log('motley-buffet: closing buffet...')
+      get('console').log('motley-buffet: closing buffet...')
       buffet_instance.close()
-      get('vendor.console').log('motley-buffet: closed buffet.')
+      get('console').log('motley-buffet: closed buffet.')
       setImmediate(cb)
     }
     else setImmediate(cb)
